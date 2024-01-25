a6792219963d92aee768a770c1fe41d02a4d6336
Emanuel Florimon <ef22-1327@unphu.edu.do>
2024-01-24 20:48:19 -0400

    Primera version del programa

---
+++ src/SimpleAgenda.java
@@ -0,0 +1,28 @@
+public class SimpleAgenda {
+public static void agregarContacto (String a){
+
+}
+public static void removerContacto (int a){
+
+}
+public static String actualizarContacto (int a) {
+    return "N/A";
+}
+    public static void mostrarContactos () {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
